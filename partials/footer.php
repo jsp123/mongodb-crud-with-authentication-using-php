054d627786edf8fd2a1a323744331c972d1ee6af
Carlo-Fontanos\CarloFontanos <carl.fontanos@gmail.com>
2016-08-21 00:16:04 +0800

    Added footer.

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,15 @@
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<span class="pull-right">Copyright &copy <a href="http://carlofontanos.com" target="_blank">www.carlofontanos.com</a>; <?php echo date('Y'); ?></span>
+					</div>
+				</div>
+				
+			</div>
+	</footer>
+	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="js/jquery.form.js"></script>
