b4a5b50dd797a9993866755daf8ea8ba51a25621
Carlo Fontanos <carl.fontanos@gmail.com>
2016-08-23 17:02:34 +0800

    Changed the title and included new scripts.

--- partials/header.php
+++ partials/header.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>MongoDB Database Manager</title>
+	<title>MongoDB CRUD Example</title>
 	
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css">
+	<link rel="stylesheet" href="css/lobibox.min.css">
 	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="override">
 	<div class="container">
-		<div class="row">
-			<nav class="navbar navbar-default">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<a class="navbar-brand" href="">MongoDB</a>
-					</div>
-					<!-- 
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#">Page 1</a></li>
-						<li><a href="#">Page 2</a></li> 
-						<li><a href="#">Page 3</a></li> 
-					</ul>
-					-->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.php">MongoDB CRUD Example</a>
 				</div>
-			</nav>
-		</div>
+				<!-- 
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Page 1</a></li>
+					<li><a href="#">Page 2</a></li> 
+					<li><a href="#">Page 3</a></li> 
+				</ul>
+				-->
+			</div>
+		</nav>
 	</div>
