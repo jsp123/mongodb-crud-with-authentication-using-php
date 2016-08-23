5fc1a92da868ab5d62b401cad11f25e89f03640b
Carlo Fontanos <carl.fontanos@gmail.com>
2016-08-23 17:03:03 +0800

    Organized the config file.

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,11 @@
 <?php 
 require_once('functions.php');
 
-// $conn = new MongoClient();
+/* Local Database Connection */
+$connection = new MongoClient();
+$db = $connection->selectDB('carlofontanos');
+
+/* Live Database Connection 
 $conn = new MongoClient('mongodb://admin:pass@ds013206.mlab.com:13206/mongoapp');
-$db = $conn->selectDB('mongoapp');
+$db = $connection->selectDB('mongoapp');
+*/
