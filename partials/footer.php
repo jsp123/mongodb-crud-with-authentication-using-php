222e439ed9cb739199ef77f6c1db4cf817e395b1
Carlo Fontanos <carl.fontanos@gmail.com>
2016-08-23 17:03:38 +0800

    Included new scripts.

--- partials/footer.php
+++ partials/footer.php
@@ -1,18 +1,20 @@
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<span class="pull-right">Copyright &copy <a href="http://carlofontanos.com" target="_blank">www.carlofontanos.com</a>; <?php echo date('Y'); ?></span>
-					</div>
+	<footer class="m-t">
+		<div class="container">	
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<span class="pull-right">Copyright &copy <a href="http://carlofontanos.com" target="_blank">www.carlofontanos.com</a>; <?php echo date('Y'); ?></span>
 				</div>
-				
 			</div>
 	</footer>
 	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="//cdn.ckeditor.com/4.5.10/standard/ckeditor.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+	<script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
+	<script src="js/lobibox.min.js"></script>
 	<script src="js/jquery.form.js"></script>
+	<script src="js/global.js"></script>
 	<script src="js/app.js"></script>
 </body>
 </html>
