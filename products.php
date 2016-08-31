a5b49990b3994c2a4df6603ea96f54d340a3528d
Carlo Fontanos <carl.fontanos@gmail.com>
2016-08-31 16:07:00 +0800

    Added template for products. This is where all user products will be displayed.

---
+++ products.php
@@ -0,0 +1,8 @@
+<?php require_once('inc/config.php'); ?>
+<?php require_once('partials/header.php'); ?>
+  
+<div class="container">
+	Products Page
+</div>
+
+<?php require_once('partials/footer.php'); ?>
