26a990cc726bcbb254a4d15df95e5855660a7953
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-02 16:47:20 +0800

    Added javascript function for reading URL variables

--- js/global.js
+++ js/global.js
@@ -45,3 +45,13 @@ function wave_box(option) {
 		}
 	}
 }
+
+function get_url_value(variable) {
+   var query = window.location.search.substring(1);
+   var vars = query.split("&");
+   for (var i=0;i<vars.length;i++) {
+		   var pair = vars[i].split("=");
+		   if(pair[0] == variable){return pair[1];}
+   }
+   return(false);
+}
