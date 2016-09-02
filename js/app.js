b71fdc89fffdedf73a476d5084b2815dc85fb130
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-02 18:05:15 +0800

    Added initial design for the product page on the front-end.

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ var app = {
 		this.init = function() {
 			// this.loaded_posts_pagination();
 			this.get_items_pagination();
+			this.get_user_items_pagination();
 			this.add_post();
 			this.update_post();
 			this.delete_post();
@@ -29,6 +30,116 @@ var app = {
 		
 		/**
 		 * Load items pagination.
+		 */
+		this.get_user_items_pagination = function() {
+			
+			_this = this;
+			
+			/* Check if our hidden form input is not empty, meaning it's not the first time viewing the page. */
+			if($('form.post-list input').val()){
+				/* Submit hidden form input value to load previous page number */
+				data = JSON.parse($('form.post-list input').val());
+				_this.ajax_get_user_items_pagination(data.page, data.th_name, data.th_sort);
+			} else {
+				/* Load first page */
+				_this.ajax_get_user_items_pagination(1, 'name', 'ASC');
+			}
+			
+			var th_active = $('.table-post-list th.active');
+			var th_name = $(th_active).attr('id');
+			var th_sort = $(th_active).hasClass('DESC') ? 'DESC': 'ASC';
+						
+			/* Search */
+			$('body').on('click', '.post_search_submit', function(){
+				_this.ajax_get_user_items_pagination(1, th_name, th_sort);
+			});
+			/* Search when Enter Key is triggered */
+			$(".post_search_text").keyup(function (e) {
+				if (e.keyCode == 13) {
+					_this.ajax_get_user_items_pagination(1, th_name, th_sort);
+				}
+			});
+			
+			/* Pagination Clicks   */                  
+			$('body').on('click', '.pagination-nav li.active', function(){
+				var page = $(this).attr('p');
+				var current_sort = $(th_active).hasClass('DESC') ? 'DESC': 'ASC';
+				_this.ajax_get_user_items_pagination(page, th_name, current_sort);                
+			}); 
+
+			/* Sorting Clicks */
+			$('body').on('click', '.table-post-list th', function(e) {
+				e.preventDefault();                             
+				var th_name = $(this).attr('id');
+													
+				if(th_name){
+					/* Remove all TH tags with an "active" class */
+					if($('.table-post-list th').removeClass('active')) {
+						/* Set "active" class to the clicked TH tag */
+						$(this).addClass('active');
+					}
+					if(!$(this).hasClass('DESC')){
+						_this.ajax_get_user_items_pagination(1, th_name, 'DESC');
+						$(this).addClass('DESC');
+					} else {
+						_this.ajax_get_user_items_pagination(1, th_name, 'ASC');
+						$(this).removeClass('DESC');
+					}
+				}
+			});
+		}
+		
+		/**
+		 * AJAX items pagination.
+		 */
+		this.ajax_get_user_items_pagination = function(page, th_name, th_sort){
+			
+			if($(".pagination-container").length > 0 && $(".products-view-user").length > 0){
+				$(".pagination-container").html('<img src="img/loading.gif" class="ml-tb" />');
+				
+				var post_data = {
+					page: page,
+					search: $('.post_search_text').val(),
+					th_name: th_name,
+					th_sort: th_sort,
+					max: $('.post_max').val(),
+				};
+				
+				$('form.post-list input').val(JSON.stringify(post_data));
+				
+				var data = {
+					action: "demo_load_my_posts",
+					data: JSON.parse($('form.post-list input').val())
+				};
+				
+				$.ajax({
+					url: 'inc/ajax/products/view.php',
+					type: 'POST',
+					data: data,
+					success: function (response) {
+						response = JSON.parse(response);
+						
+						if($(".pagination-container").html(response.content)){
+							$('.pagination-nav').html(response.navigation);
+							$('.table-post-list th').each(function() {
+								/* Append the button indicator */
+								$(this).find('span.glyphicon').remove();    
+								if($(this).hasClass('active')){
+									if(JSON.parse($('form.post-list input').val()).th_sort == 'DESC'){
+										$(this).append(' <span class="glyphicon glyphicon-chevron-down pull-right"></span>');
+									} else {
+										$(this).append(' <span class="glyphicon glyphicon-chevron-up pull-right"></span>');
+									}
+								}
+							});
+						}
+					}
+				});
+			}
+		}
+		
+		/**
+		 * Load items pagination on the front-end.
 		 */
 		this.get_items_pagination = function() {
 			
@@ -93,7 +204,7 @@ var app = {
 		 */
 		this.ajax_get_items_pagination = function(page, th_name, th_sort){
 			
-			if($(".pagination-container").length){
+			if($(".pagination-container").length > 0 && $('.products-view').length > 0 ){
 				$(".pagination-container").html('<img src="img/loading.gif" class="ml-tb" />');
 				
 				var post_data = {
@@ -112,7 +223,7 @@ var app = {
 				};
 				
 				$.ajax({
-					url: 'inc/ajax/products/view.php',
+					url: 'inc/ajax/products/view-front.php',
 					type: 'POST',
 					data: data,
 					success: function (response) {
@@ -283,14 +394,14 @@ var app = {
 				e.preventDefault();
 				wave_box('on');
 				
+				var _this = this;
+				var image_featured_id = this.id.split('-')[1];
+					
 				if( $(this).hasClass('glyphicon-star') ){
 					Lobibox.notify('error', {msg: 'The image you clicked is already the featured image.', size: 'mini', sound: false});
 					wave_box('off');
 					
 				} else {
-					var image_featured = '#' + this.id;
-					var image_featured_id = this.id.split('-')[1];
-					
 					$.ajax({
 						url: 'inc/ajax/products/image.php',
 						type: 'POST',
@@ -305,7 +416,7 @@ var app = {
 							
 							if(response.status == '1'){
 								if($('.images-section').find('span.glyphicon-star').switchClass('glyphicon-star', 'glyphicon-star-empty').removeAttr('style')){
-									$(image_featured).switchClass('glyphicon-star-empty', 'glyphicon-star').css('color', '#E4C317');
+									$(_this).switchClass('glyphicon-star-empty', 'glyphicon-star').css('color', '#E4C317');
 									Lobibox.notify('success', {msg: response.message, size: 'mini', sound: false});
 								}
 							} else {
--- products.php
+++ products.php
@@ -1,8 +1,33 @@
 <?php require_once('inc/config.php'); ?>
 <?php require_once('partials/header.php'); ?>
   
-<div class="container">
-	Products Page
+<div class="container products-view">
+	<form class = "post-list">
+		<input type = "hidden" value = "" />
+	</form>
+	
+	<div class="clearfix">
+		<article class="navbar-form navbar-left p-0 m-0 ml-b">
+			<div class="form-group">
+				<label>Per Page: </label>
+				<select class="form-control post_max">
+					<option value="40">40</option>
+					<option value="80">80</option>
+					<option value="160">160</option>
+				</select>
+				<label>Search Keyword: </label>
+				<input type="text" class="form-control post_search_text" placeholder="Enter a keyword">
+			</div>
+			<input type = "submit" value = "Filter" class = "btn btn-primary post_search_submit" />
+		</article>
+	</div>
+	
+	<hr />
+	
+	<div class="clearfix">
+		<div class="pagination-container clearfix"></div>
+		<div class="pagination-nav"></div>
+	</div>
 </div>
 
 <?php require_once('partials/footer.php'); ?>
