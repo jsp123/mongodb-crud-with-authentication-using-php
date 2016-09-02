829b01034560db8ce7f39e0db52053bf8e9e9fde
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-02 13:09:55 +0800

    Included jqueryUI to leverage some useful effects functions.

--- partials/footer.php
+++ partials/footer.php
@@ -8,6 +8,7 @@
 	</footer>
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="//cdn.ckeditor.com/4.5.10/standard/ckeditor.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
