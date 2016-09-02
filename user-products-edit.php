ed2179fd4fade104b8429fa22b473f143d1675d1
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-02 15:59:57 +0800

    Removed is_set() checker.

--- user-products-edit.php
+++ user-products-edit.php
@@ -16,7 +16,21 @@
 	<?php
 	/* Check if we recevied a valid object id */
 	if( MongoId::isValid( $item_id ) ):	
-		$item = get_product( $item_id );
+		$item = array();
+		$item_data = get_product( $item_id );
+		
+		/* List down all known fields for our document */
+		/* If you wish to add more fields, make sure to populate them here */
+		/* This makes NoSQL scalability a breeze! */
+		$fields = array('name', 'content', 'excerpt', 'price', 'status', 'date', 'quantity', 'images', 'featured_image'); 
+		
+		/* Let's check each fields if they have a matching column in our document */
+		/* If the fields are not created yet, at least we could easily set a empty string as their temporary value */
+		foreach( $fields as $field ){
+			$item[$field] = isset( $item_data->$field ) ? $item_data->$field : '';
+		}
+		
+		$item = (object) $item;
 	?>
 		<p class="ml-b"><a href = "user-products.php" class="ml-b text-success"><span class="glyphicon glyphicon-chevron-left"></span> All products</a></p>
 		
@@ -31,15 +45,15 @@
 					<div class="col-md-8">
 						<div class="form-group">
 							<label>Name</label>
-							<input type="text" class="form-control" name="name" value="<?php echo is_set( $item->name ); ?>" />
+							<input type="text" class="form-control" name="name" value="<?php echo $item->name; ?>" />
 						</div>
 						<div class="form-group">
 							<label>Description</label>
-							<textarea id="ck-editor-area" class="form-control" name="content"><?php echo is_set( $item->content ); ?></textarea>
+							<textarea id="ck-editor-area" class="form-control" name="content"><?php echo $item->content; ?></textarea>
 						</div>
 						<div class="form-group">
 							<label>Short Description</label>
-							<textarea class="form-control" name="excerpt" rows="7"><?php echo is_set( $item->excerpt ); ?></textarea>
+							<textarea class="form-control" name="excerpt" rows="7"><?php echo $item->excerpt; ?></textarea>
 						</div>
 						<div class="form-group ml-t">
 							<label>Upload Images</label>
@@ -47,12 +61,12 @@
 							<hr />
 							
 							<div class = "clearfix m-t images-section">
-								<?php if( is_set( $item->images ) ): ?>
+								<?php if( $item->images ): ?>
 									<?php $image_count = 1; ?>
 									<?php foreach( $item->images as $image ): ?>
 										<div class = "col-sm-3">
 											<span class="unset-image glyphicon glyphicon-remove text-danger lead m-0 c-p" id="unset-<?php echo $image; ?>" title="Delete image"></span>
-											<?php if( is_set( $item->featured_image ) == $image ): ?>
+											<?php if( $item->featured_image == $image ): ?>
 												<span class="set-featured-image glyphicon glyphicon-star lead m-0 c-p" title="Set as featured image" id="featured-<?php echo $image; ?>" style="color: #E4C317"></span>
 											<?php else: ?>
 												<span class="set-featured-image glyphicon glyphicon-star-empty lead m-0 c-p" title="Set as featured image" id="featured-<?php echo $image; ?>"></span>
@@ -77,20 +91,20 @@
 							<label>Price</label>
 							<div class="input-group">
 								<div class="input-group-addon">$</div>
-								<input type="text" class="form-control" name="price" value="<?php echo is_set( $item->price ); ?>" placeholder="Amount">
+								<input type="text" class="form-control" name="price" value="<?php echo $item->price; ?>" placeholder="Amount">
 							</div>
 						</div>
 						<div class="form-group">
 							<label>Status</label>
 							<select class="form-control" name="status">
-								<option value="1" <?php echo is_set( $item->status ) == 1 ? 'selected' : ''; ?>>Active</option>
-								<option value="0" <?php echo is_set( $item->status ) == 0 ? 'selected' : ''; ?>>Inactive</option>
+								<option value="1" <?php echo $item->status == 1 ? 'selected' : ''; ?>>Active</option>
+								<option value="0" <?php echo $item->status == 0 ? 'selected' : ''; ?>>Inactive</option>
 							</select>
 						</div>
 						<div class="form-group">
 							<label>Date</label>
 							<div class='input-group date datepicker'>
-								<input type='text' class="form-control" name="date" value="<?php echo is_set( $item->date ); ?>" />
+								<input type='text' class="form-control" name="date" value="<?php echo $item->date; ?>" />
 								<span class="input-group-addon">
 									<span class="glyphicon glyphicon-calendar"></span>
 								</span>
@@ -98,7 +112,7 @@
 						</div>
 						<div class="form-group">
 							<label>Quantity</label>
-							<input type="number" class="form-control" name="quantity" value="<?php echo is_set( $item->quantity ); ?>" />
+							<input type="number" class="form-control" name="quantity" value="<?php echo $item->quantity; ?>" />
 						</div>
 						<input type="submit" class="btn btn-success" value="Update" />
 					</div>
